d3a89c8c6b8850f4236c01030bc9dbae501b0c68
Evelyn <evelyn.sun1@gmail.com>
2016-08-31 18:21:47 +0000

    modified

--- app.js
+++ app.js
@@ -25,7 +25,7 @@
  /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({ path: '.env' });
+dotenv.load({ path: '.env.example' });
 dotenv.config({silent: true})
 
  /**
