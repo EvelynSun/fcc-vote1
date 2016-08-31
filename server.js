ea453997e63048bf2d7256afa406986236f0fdcc
Evelyn <evelyn.sun1@gmail.com>
2016-08-31 17:51:16 +0000

    modified

--- server.js
+++ server.js
@@ -24,7 +24,7 @@
  /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({ path: '.env.example' });
+dotenv.load({ path: '.env' });
  /**
   * Controllers(route handlers)
   *
